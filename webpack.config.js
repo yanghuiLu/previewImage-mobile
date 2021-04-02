018638693f2a42ac276788f67354bddc92607eba
joey <603922507@qq.com>
2021-04-02 19:23:06 +0800

    first

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+
+
+module.exports = {
+    entry: {
+        // vendors: [],
+        main: {
+            import: './src/main.js',
+            // filename: 'pages/[name][ext]',
+            // dependOn: ['vendors']
+        },
+    },
+}
